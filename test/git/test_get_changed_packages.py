99ceed1bb1e83617969f644cc8ce2226e2aa7f35
r-kells <ryan.kelly@kik.com>
2018-11-24 01:48:59 -0500

    re-arrage readme content for clarity

--- test/git/test_get_changed_packages.py
+++ test/git/test_get_changed_packages.py
@@ -12,6 +12,7 @@ TMP_DIR = os.path.join(PARENT_DIR, 'tmp')
 
 
 class TestChangedPackages(unittest.TestCase):
+
     @classmethod
     def setUp(cls):
         if os.path.isdir(TMP_DIR):
@@ -20,11 +21,6 @@ class TestChangedPackages(unittest.TestCase):
         os.mkdir(TMP_DIR)
         with chdir(TMP_DIR):
             scream.init_monorepo(TMP_DIR)
-            subprocess.call(["git", "init"])
-
-            package_name = "packagea"
-            package_dir = os.path.join(TMP_DIR, package_name)
-            scream.new_package(package_dir, namespaces=['company'], package_name=package_name)
 
     @classmethod
     def tearDown(cls):
@@ -43,11 +39,42 @@ class TestChangedPackages(unittest.TestCase):
     def test_no_changed_packages(self):
         with chdir(TMP_DIR):
             parent_branch = detect_changed_packages.get_parent_branch()
-            changed_files = detect_changed_packages.get_changed_files(parent_branch)
+            changed_files = detect_changed_packages.get_changed_files(
+                parent_branch)
 
         self.assertEqual(changed_files, [], parent_branch)
 
+    def test_changed_packages(self):
+        packagea_name = "packagea"
+        packagea_dir = os.path.join(TMP_DIR, packagea_name)
 
-# subprocess.call(["git", "checkout", "-b", parent_branch])
-# subprocess.call(["git", "add", "."])
-# subprocess.call(["git", "commit", "-m", "ok"])
+        expected_changes = [
+            ['A', 'packagea/README.md'],
+            ['A', 'packagea/company/__init__.py'],
+            ['A', 'packagea/company/packagea/__init__.py'],
+            ['A', 'packagea/company/packagea/module.py'],
+            ['A', 'packagea/setup.cfg'],
+            ['A', 'packagea/setup.py'],
+            ['A', 'packagea/tests/__init__.py'],
+            ['A', 'packagea/tests/test_module.py']
+        ]
+
+        with chdir(TMP_DIR):
+            # Add a package.
+            scream.new_package(
+                packagea_dir,
+                namespaces=['company'],
+                package_name=packagea_name)
+
+            subprocess.call(["git", "add", "."])
+
+            parent_branch = detect_changed_packages.get_parent_branch()
+            changed_files = detect_changed_packages.get_changed_files(parent_branch)
+
+        self.assertEqual(changed_files, expected_changes, parent_branch)
+
+    def test_parse_git_diff(self):
+        input_example = """A	packagea/README.md\nA	packageb/README.md"""
+        expected = [['A', "packagea/README.md"], ["A", "packageb/README.md"]]
+        actual = detect_changed_packages.parse_git_diff(input_example)
+        self.assertEqual(expected, actual)
